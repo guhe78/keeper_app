55f3cb57b51a266a502880b6c290ee3cc1ca0416
gh1978@hotmail.de <gh1978@hotmail.de>
2025-06-08 18:27:48 +0200

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>Content Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        omnis corporis velit, vitae obcaecati placeat, quibusdam mollitia sed
+        animi suscipit amet minima aut sapiente architecto est saepe illum quam
+        odit dignissimos molestiae! Minima eaque nisi, aliquid illo odio
+        mollitia dicta, vero laudantium est dolorem accusamus rerum fugiat
+        delectus repellendus voluptatibus?
+      </p>
+    </div>
+  );
+}
+
+export default Note;

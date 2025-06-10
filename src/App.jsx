3e4019c5142bf8ba963871620f3f75cf5abe12ce
guhe78 <gh1978@hotmail.de>
2025-06-10 11:48:47 +0200

    add notes.js and props and map

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
-   return (
-      <div>
-         <Header />
-         <Note />
-         <Footer />
-      </div>
-   );
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
   return (
-    <div>
-      <h1>Content Title</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        omnis corporis velit, vitae obcaecati placeat, quibusdam mollitia sed
-        animi suscipit amet minima aut sapiente architecto est saepe illum quam
-        odit dignissimos molestiae! Minima eaque nisi, aliquid illo odio
-        mollitia dicta, vero laudantium est dolorem accusamus rerum fugiat
-        delectus repellendus voluptatibus?
-      </p>
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
